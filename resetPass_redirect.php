647a046bd051ed3b777f4d112704074e4d296c93
hetsuthar028 <hetmewada028@gmail.com>
2020-05-07 21:48:50 +0530

    More Security features added
    Password Recovery, Resetting password

---
+++ resetPass_redirect.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="resetPass_redirect_php.php">
+            <input type="text" name="newPassword" required>
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
